bc50bd1f66dd834a4ec0256700414db8da690fd9
Nora Gully <nora.gully@gmail.com>
2017-12-05 14:55:37 -0700

    Frogger-test.js file working, add preliminary tests of object

--- test/Frogger-test.js
+++ test/Frogger-test.js
@@ -1,32 +1,100 @@
 const { expect } = require('chai');
 const { assert } = require('chai');
 
-const Frogger = require('..lib/Frogger.js');
-const Board = require('..lib/Board.js');
-const Car = require('..lib/Car.js');
-const Log = require('..lib/Log.js');
-const Movers = require('..lib/Movers.js');
+const Frogger = require('../lib/Frogger.js');
+const Board = require('../lib/Board.js');
+const Car = require('../lib/Car.js');
+const Log = require('../lib/Log.js');
+const Movers = require('../lib/Movers.js');
+
+
 
 describe('Frogger', function() {
-  var frogger = new Frogger();
-  var board = new Board();
+  let frogger;
+  let car;
 
-
-  it('should be a function', () => {
-    expect(true, true);
+  beforeEach(() => {
+    frogger = new Frogger();
+    car = new Car();
   })
 
-  it.skip('should be an instance of a Toad', () => {
-    expect(frogger).to.be.an.instanceOf(Frogger);
+  it('should be a object', () => {
+    assert.equal(typeof(frogger), 'object');
   })
 
-  // it('should have moved by x and y', () => {
+  it('should be in its start position', () => {
+    assert.equal(frogger.x, 280);
+    assert.equal(frogger.y, 560);
+  })
 
-// frogger.move(event.keyCode) check that this.x has incremented by the width
-//   }
+  it('x should increment when we hit right arrow', () =>{
+    let event = {
+      keyCode: 39
+    }
+    assert.equal(frogger.x, 280);
+    assert.equal(frogger.y, 560);
+    frogger.move(event);
+    assert.equal(frogger.x, 320);
+    assert.equal(frogger.y, 560);
+  })
 
-}
+  it('y should increment when we hit down arrow', () => {
+    let event = {
+      keyCode: 40
+    }
+    assert.equal(frogger.x, 280);
+    assert.equal(frogger.y, 560);
+    frogger.move(event);
+    assert.equal(frogger.x, 280);
+    assert.equal(frogger.y, 600);
+  })
+
+  it('x should decrement when we hit left arrow', () =>{
+    let event = {
+      keyCode: 37
+    }
+    assert.equal(frogger.x, 280);
+    assert.equal(frogger.y, 560);
+    frogger.move(event);
+    assert.equal(frogger.x, 240);
+    assert.equal(frogger.y, 560);
+  })
+
+
+  it('y should decrement when we hit up arrow', () => {
+    let event = {
+      keyCode: 38
+    }
+    assert.equal(frogger.x, 280);
+    assert.equal(frogger.y, 560);
+    frogger.move(event);
+    assert.equal(frogger.x, 280);
+    assert.equal(frogger.y, 520);
+  })
+
+  it('should not move when you hit any other key', () => {
+    let event = {
+      keyCode: 36
+    }
+    assert.equal(frogger.x, 280);
+    assert.equal(frogger.y, 560);
+    frogger.move(event);
+    assert.equal(frogger.x, 280);
+    assert.equal(frogger.y, 560);
+    assert.equal(frogger.move(event), null);
+    })
+
+})
+  
+
 //event object with keycode and give that to move()
 //test every interaction
 //test every key code
 //need to create new frog and new car and with same x, y 
+
+// const canvas = class Canvas {
+//   constructor(){
+//     this.width = 600;
+//     this.height = 680;
+//   }
+// }
