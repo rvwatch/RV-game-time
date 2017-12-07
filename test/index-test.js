3b388ebff0fabbe454b7bd1cc7d7c4a8c1cfea59
Ricardo Viera <ricardoclean@gmail.com>
2017-12-07 09:05:30 -0700

    Add testing to make sure objects loop to the other side of the canvas properly

---
+++ test/index-test.js
@@ -0,0 +1,15 @@
+const { assert } = require('chai');
+
+
+
+describe('index', function() {
+  
+
+  it('should increase the velocity of all objects on new level', () => {
+    assert.equal();
+
+
+    
+  })
+
+});
